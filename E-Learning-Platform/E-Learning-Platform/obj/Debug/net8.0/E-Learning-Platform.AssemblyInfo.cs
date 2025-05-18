9d09e604126bdab20c6e4f1eaf85eb26bb359f88
paccyTech <paccyTech>
2025-05-18 13:23:18 +0200

    Edit module

--- E-Learning-Platform/E-Learning-Platform/obj/Debug/net8.0/E-Learning-Platform.AssemblyInfo.cs
+++ E-Learning-Platform/E-Learning-Platform/obj/Debug/net8.0/E-Learning-Platform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Learning-Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f22cde4901fcd93fb1725b58411fe844eb605f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba1129d438c0ef33b2df3b85f49ebc6cf3095754")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Learning-Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Learning-Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
