3afb81e4c8526b3c53d0a67d13a97bb0330e531e
paccyTech <paccyTech>
2025-05-18 12:11:32 +0200

    AddQ

---
+++ E-Learning-Platform/E-Learning-Platform/Pages/Instructor/Content/AddQuestion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Learning_Platform.Pages.Instructor.Content
+{
+    public class AddQuestionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
