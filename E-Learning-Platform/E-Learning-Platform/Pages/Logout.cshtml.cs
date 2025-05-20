bb7858744804e8e12d68ee06c3ad845300db41a2
Melissa <dushimelisa22@gmail.com>
2025-05-20 02:51:54 -0700

    logout1

--- E-Learning-Platform/E-Learning-Platform/Pages/Logout.cshtml.cs
+++ E-Learning-Platform/E-Learning-Platform/Pages/Logout.cshtml.cs
@@ -1,12 +1,6 @@
+// Create a LogoutModel.cs in Pages folder
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace E_Learning_Platform.Pages
-{
-    public class LogoutModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
-}
