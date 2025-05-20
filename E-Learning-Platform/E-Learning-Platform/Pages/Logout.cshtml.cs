57e87b490e085f5add0fd1e9e596f7b530d48bdf
Melissa <dushimelisa22@gmail.com>
2025-05-20 02:54:14 -0700

    logout2

--- E-Learning-Platform/E-Learning-Platform/Pages/Logout.cshtml.cs
+++ E-Learning-Platform/E-Learning-Platform/Pages/Logout.cshtml.cs
@@ -4,3 +4,15 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace E_Learning_Platform.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.Session.Clear();
+            return RedirectToPage("/Login");
+        }
+    }
+}
